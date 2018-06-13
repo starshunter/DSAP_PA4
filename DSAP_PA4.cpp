eda02d7a16b83dadc1686ea7e75f5e6f6a9e96d5
Starshunter <petercheng0303@gmail.com>
2018-06-14 00:33:46 +0800

    Implement all the process without departure and change line.

--- DSAP_PA4.cpp
+++ DSAP_PA4.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-int time_to_second(char t[]);
+int time_to_second(string t);
+string second_to_time(int t);
 
 template <typename T>
 class Node
@@ -118,21 +119,29 @@ class Customer
 {
 public:
     string name;
+    int arrive_time;
     int start_time;
     int end_time;
     int time_need;
     bool business;
+    int wait;
     Customer();
-    Customer(string name,int start_time,int time_need,bool business);
+    Customer(string name,int arrive_time,int time_need,bool business);
+    bool operator>(const Customer &c);
+    bool operator<(const Customer &c);
+    bool operator>=(const Customer &c);
+    bool operator<=(const Customer &c);
+    bool operator==(const Customer &c);
 };
 
 class Event
 {
 public:
     string name;
+    int start_time;
     int left_time;
     Event();
-    Event(string name,int left_time);
+    Event(string name,int start_time,int left_time);
     bool operator>(const Event &ev);
     bool operator<(const Event &ev);
     bool operator>=(const Event &ev);
@@ -303,10 +312,10 @@ void ArrayMaxHeap<T>::heapRebuild(int subTreeRootIndex)
         if(getRightChildIndex(subTreeRootIndex)!=0)
         {
             int rightChildIndex=largerChildIndex+1;
-            if (Items[rightChildIndex]>Items[largerChildIndex])
+            if (Items[rightChildIndex]<Items[largerChildIndex])
                 largerChildIndex=rightChildIndex;
         }
-        if(Items[subTreeRootIndex]<=Items[largerChildIndex])
+        if(Items[subTreeRootIndex]>Items[largerChildIndex])
         {
             swap(Items[subTreeRootIndex],Items[largerChildIndex]);
             heapRebuild(largerChildIndex);
@@ -374,7 +383,7 @@ bool ArrayMaxHeap<T>::add(const T& newData)
     while(newDataIndex>0&&!inPlace)
     {
         int parentIndex=(newDataIndex-1)/2;
-        if (Items[newDataIndex]<=Items[parentIndex])
+        if (Items[newDataIndex]>Items[parentIndex])
             inPlace=true;
         else
         {
@@ -440,25 +449,48 @@ T Heap_PriorityQueue<T>::peek()const throw(runtime_error)
 
 //Customer================================================================================================
 
-Customer::Customer():name(""),start_time(0),end_time(0),time_need(0),business(false){}
+Customer::Customer():name(""),arrive_time(0),start_time(0),end_time(0),time_need(0),business(false),wait(0){}
 
-Customer::Customer(string name,int start_time,int time_need,bool business)
+Customer::Customer(string name,int arrive_time,int time_need,bool business)
 {
     this->name=name;
+    this->arrive_time=arrive_time;
     this->start_time=start_time;
     this->time_need=time_need;
     this->business=business;
+    this->wait=0;
+}
+bool Customer::operator>(const Customer &c)
+{
+    return(this->end_time>c.end_time);
+}
+bool Customer::operator<(const Customer &c)
+{
+    return(this->end_time<c.end_time);
+}
+bool Customer::operator>=(const Customer &c)
+{
+    return(this->end_time>=c.end_time);
+}
+bool Customer::operator<=(const Customer &c)
+{
+    return(this->end_time<=c.end_time);
+}
+bool Customer::operator==(const Customer &c)
+{
+    return(this->end_time==c.end_time);
 }
 
 //Customer================================================================================================
 
 //Event===================================================================================================
 
-Event::Event():name(""),left_time(0){}
+Event::Event():name(""),start_time(0),left_time(0){}
 
-Event::Event(string name,int left_time)
+Event::Event(string name,int start_time,int left_time)
 {
     this->name=name;
+    this->start_time=start_time;
     this->left_time=left_time;
 }
 
@@ -490,15 +522,21 @@ bool Event::operator==(const Event &ev)
 //Event===================================================================================================
 int main()
 {
-    int n,m;
+    int n,m,total_time=0,customer_num=0;
+
     cin>>m>>n;
+
     LinkedQueue<Customer> *normal_line,*business_line;
     Heap_PriorityQueue<Event> event_list;
+    Heap_PriorityQueue<Customer> customer_list;
+
     bool *normal_counter,*business_counter;
+
     normal_line=new LinkedQueue<Customer>[m];
     business_line=new LinkedQueue<Customer>[n];
     normal_counter=new bool[m];
     business_counter=new bool[n];
+
     for(int i=0;i<m;i++)
         normal_counter[i]=false;
     for(int i=0;i<n;i++)
@@ -507,72 +545,122 @@ int main()
     cin.get();
     while(cin.getline(statement,sizeof(statement)))
     {
+        if(strlen(statement)==0)
+            break;
         char *cut=nullptr;
-        int start_time;
-        string code,name;
+        int arrive_time;
+        char *code,*name;
         cut=strtok(statement," ");
-        start_time=time_to_second(cut);
-        cut=strtok(NULL," ");
-        code=cut;
-        if(!code.compare("A"))
+        string st=cut;
+        arrive_time=time_to_second(st);
+
+        if(!event_list.isEmpty())
         {
-            string type;
+            while(arrive_time>=event_list.peek().left_time)
+            {
+                for(int i=0;i<m+n;i++)
+                {
+                    if(i<n)
+                    {
+                        if(business_counter[i]&&!business_line[i].peekFront().name.compare(event_list.peek().name))
+                        {
+                            Customer temp=business_line[i].peekFront();
+                            temp.wait+=event_list.peek().start_time-temp.arrive_time;
+                            total_time+=temp.wait;
+                            business_line[i].dequeue();
+                            customer_num++;
+                            temp.start_time=event_list.peek().start_time;
+                            temp.end_time=event_list.peek().left_time;
+                            customer_list.add(temp);
+                            event_list.remove();
+                            if(!business_line[i].isEmpty())
+                            {
+                                Event ev(business_line[i].peekFront().name,temp.end_time,temp.end_time+business_line[i].peekFront().time_need);
+                                event_list.add(ev);
+                            }
+                            else
+                                business_counter[i]=false;
+                        }
+                    }
+                    else
+                    {
+                        if(normal_counter[i-n]&&!normal_line[i-n].peekFront().name.compare(event_list.peek().name))
+                        {
+                            Customer temp=normal_line[i-n].peekFront();
+                            temp.wait+=event_list.peek().start_time-temp.arrive_time;
+                            total_time+=temp.wait;
+                            normal_line[i-n].dequeue();
+                            customer_num++;
+                            temp.start_time=event_list.peek().start_time;
+                            temp.end_time=event_list.peek().left_time;
+                            customer_list.add(temp);
+                            event_list.remove();
+                            if(!normal_line[i-n].isEmpty())
+                            {
+                                Event ev(normal_line[i-n].peekFront().name,temp.end_time,temp.end_time+normal_line[i-n].peekFront().time_need);
+                                event_list.add(ev);
+                            }
+                            else
+                                normal_counter[i-n]=false;
+                        }
+                    }
+                }
+                if(event_list.isEmpty())
+                    break;
+            }
+        }
+        code=strtok(NULL," ");
+        if(!strcmp(code,"A"))
+        {
+            char *type;
             int time_need;
             bool business=true;
-            cut=strtok(NULL," ");
-            name=cut;
-            cut=strtok(NULL," ");
-            type=cut;
+            name=strtok(NULL," ");
+            type=strtok(NULL," ");
             cut=strtok(NULL," ");
             time_need=stoi(cut);
-            if(type.compare("B"))
+            if(strcmp(type,"B"))
                 business=false;
-            Customer cus(name,start_time,time_need,business);
+            Customer cus(name,arrive_time,time_need,business);
             int short_lengh=1000000,short_id=-1;
             for(int i=0;i<n+m;i++)
             {
-                if(!cus.business&&i<m)
+                if(!cus.business&&i<n)
                     continue;
                 int num=0;
-                if(i>=m)
-                {
-                    num+=normal_line[i-m].get_size();
-                    if(normal_counter[i-m])
-                        num++;
-                }
+                if(i>=n)
+                    num+=normal_line[i-n].get_size();
                 else
-                {
                     num+=business_line[i].get_size();
-                    if(business_counter[i])
-                        num++;
-                }
                 if(num<short_lengh)
                     short_lengh=num,short_id=i;
             }
-            if(short_id>=m)
+            if(short_id>=n)
             {
-                short_id-=m;
-                normal_line[short_id].enqueue(cus);
+                short_id-=n;
                 if(!normal_counter[short_id])
                 {
                     normal_counter[short_id]=true;
-                    Event ev(name,start_time+time_need);
+                    Event ev(name,arrive_time,arrive_time+time_need);
                     event_list.add(ev);
+                    cus.start_time=arrive_time;
                 }
+                normal_line[short_id].enqueue(cus);
             }
             else
             {
-                business_line[short_id].enqueue(cus);
                 if(!business_counter[short_id])
                 {
                     business_counter[short_id]=true;
-                    Event ev(name,start_time+time_need);
+                    Event ev(name,arrive_time,arrive_time+time_need);
                     event_list.add(ev);
+                    cus.start_time=arrive_time;
                 }
+                business_line[short_id].enqueue(cus);
             }
 
         }
-        else if(!code.compare("D"))
+        else if(!strcmp(code,"D"))
         {
             cut=strtok(NULL," ");
             name=cut;
@@ -583,18 +671,123 @@ int main()
             name=cut;
         }
     }
+    if(!event_list.isEmpty())
+    {
+        while(!event_list.isEmpty())
+        {
+            for(int i=0;i<m+n;i++)
+            {
+                if(i<n)
+                {
+                    if(business_counter[i]&&!business_line[i].peekFront().name.compare(event_list.peek().name))
+                    {
+                        Customer temp=business_line[i].peekFront();
+                        temp.wait+=event_list.peek().start_time-temp.arrive_time;
+                        total_time+=temp.wait;
+                        business_line[i].dequeue();
+                        customer_num++;
+                        temp.start_time=event_list.peek().start_time;
+                        temp.end_time=event_list.peek().left_time;
+                        customer_list.add(temp);
+                        if(!business_line[i].isEmpty())
+                        {
+                            Event ev(business_line[i].peekFront().name,event_list.peek().left_time,event_list.peek().left_time+business_line[i].peekFront().time_need);
+                            event_list.remove();
+                            event_list.add(ev);
+                        }
+                        else
+                        {
+                            business_counter[i]=false;
+                            event_list.remove();
+                        }
+                    }
+                }
+                else
+                {
+                    if(normal_counter[i-n]&&!normal_line[i-n].peekFront().name.compare(event_list.peek().name))
+                    {
+                        Customer temp=normal_line[i-n].peekFront();
+                        temp.wait+=event_list.peek().start_time-temp.arrive_time;
+                        total_time+=temp.wait;
+                        normal_line[i-n].dequeue();
+                        customer_num++;
+                        temp.start_time=event_list.peek().start_time;
+                        temp.end_time=event_list.peek().left_time;
+                        customer_list.add(temp);
+                        if(!normal_line[i-n].isEmpty())
+                        {
+                            Event ev(normal_line[i-n].peekFront().name,event_list.peek().left_time,event_list.peek().left_time+normal_line[i-n].peekFront().time_need);
+                            event_list.remove();
+                            event_list.add(ev);
+                        }
+                        else
+                        {
+                            normal_counter[i-n]=false;
+                            event_list.remove();
+                        }
+                    }
+                }
+            }
+            if(event_list.isEmpty())
+                break;
+        }
+    }
+    while(!customer_list.isEmpty())
+    {
+        Customer temp=customer_list.peek();
+        cout<<temp.name<<" "<<second_to_time(temp.start_time)<<" "<<second_to_time(temp.end_time)<<endl;
+        customer_list.remove();
+    }
+    double avg=(double)total_time/customer_num;
+    cout<<round(avg)<<endl;
     return 0;
 }
 
-int time_to_second(char t[])
+int time_to_second(string t)
 {
-    char *c=nullptr;
+    string tt=t;
     int h,m,s;
-    c=strtok(t,":");
-    h=stoi(c);
-    c=strtok(NULL,":");
-    m=stoi(c);
-    c=strtok(NULL,":");
-    s=stoi(c);
+    h=stoi(t.substr(0,2));
+    m=stoi(t.substr(3,2));
+    s=stoi(t.substr(6,2));
     return 3600*h+60*m+s;
+}
+
+string second_to_time(int t)
+{
+    string time_tag="";
+    int h=t/3600;
+    if(h<10)
+    {
+        time_tag.append("0");
+        time_tag.append(to_string(h));
+        time_tag.append(":");
+    }
+    else
+    {
+        time_tag.append(to_string(h));
+        time_tag.append(":");
+    }
+    t=t%3600;
+    int m=t/60;
+    int s=t%60;
+    if(m<10)
+    {
+        time_tag.append("0");
+        time_tag.append(to_string(m));
+        time_tag.append(":");
+    }
+    else
+    {
+        time_tag.append(to_string(m));
+        time_tag.append(":");
+    }
+    if(s<10)
+    {
+        time_tag.append("0");
+        time_tag.append(to_string(s));
+    }
+    else
+        time_tag.append(to_string(s));
+    return time_tag;
 }
